a04b03f8eaba64e1c94a679753fa0833b5ae17c1
Srbaqua <23bcs101@nith.ac.in>
2025-06-23 03:16:41 +0530

    userData creation and storing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT  = process.env.port || 5000;
+const bodyParser = require('body-parser');
+const signupRoute = require('./routes/signup');
 
-
+app.use(bodyParser.json());
+app.use("/user",signupRoute);
 
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`);
--- frontend/src/pages/header/header.js
+++ frontend/src/pages/header/header.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const header = () => {
+const Header = () => {
   return (
-    <div>
-      header
-    </div>
-  )
-}
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand><strong>Not LoggedIn</strong></Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/login" className="nav-link">
+            Login
+          </Nav.Link>
+          <Nav.Link as={Link} to="/signup" className="nav-link">
+            Signup
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default header
+export default Header;
